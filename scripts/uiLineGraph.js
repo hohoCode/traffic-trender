ccd92a66fd5688b6f183bdae02d6d4cc174b03ee
Richard <rbjohns8@cs.umd.edu>
2012-11-10 00:09:46 -0500

    dynatree

--- scripts/uiLineGraph.js
+++ scripts/uiLineGraph.js
@@ -8,11 +8,17 @@ var uiLineGraph = uiLineGraph || {}; // namespace
 $(document).ready(function(){
     $("#uiLineGraphPanel").panel({
         stackable:true
-    });    
+    });
+    
+    $("#uiLineGraph_impactFactor").change(uiTreeMap.change);
+    $("#uiLineGraph_maximumLength").change(uiTreeMap.change);
+    $("#uiLineGraph_time").change(uiTreeMap.change);
 });
 
 uiTreeMap.change = function() {
 	rb1 = $("#uiTreeMap_impactFactor").val();
 	rb2 = $("#uiTreeMap_maximumLength").val();
 	rb3 = $("#uiTreeMap_time").val();
+	
+	/* update line graph here */
 }
