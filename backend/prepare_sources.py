80f26acbc99e4fa5b7ec3f8d38f83dd360d46371
Chris Musialek <christopher.musialek@gsa.gov>
2012-11-11 19:22:00 -0500

    Oops, forgot you need to explicit add - here is the function to build the json file

--- backend/prepare_sources.py
+++ backend/prepare_sources.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 import csv
+import json
 
-if __name__ == "__main__":
+def prepare_basic_source_data_csv():
 	csvfileout = open('sources/aggregated_bottlenecks_2010-2011_NC_SC_VA_MD_prepared.csv', 'wb')
 	csvwriter = csv.writer(csvfileout)
 	with open('sources/aggregated_bottlenecks_2010-2011_NC_SC_VA_MD.csv', 'rb') as csvfile:
@@ -20,4 +21,40 @@ if __name__ == "__main__":
 			road_name = row[2].split('@')[0].strip()
 			row.append(road_name)
 			csvwriter.writerow(row)
-
+def prepare_filter_menu_bigO_json():
+	jsonfilename = 'sources/filter_menu_data.json'
+	csvroads = open('sources/unique_state_county_roadname', 'rb')
+	roadreader = csv.reader(csvroads, delimiter='\t')
+	h = {}
+	states = []
+	for row in roadreader:
+		key = ",".join(row)
+		stateName = row[0]
+		countyName = row[1]
+		roadName = row[2]
+		cur = {'title': roadName, 'select': 'true', 'key': key}
+		countyState = stateName + "-" + countyName
+		if not h.has_key(stateName):
+			#create state object container
+			curstate = {'title': stateName, 'key': stateName, 'children': []}
+			states.append(curstate)
+			#store id where state is kept
+			h[stateName] = len(states) - 1
+		if not h.has_key(countyState):
+			#create county object container
+			curcounty = {'title': countyState, 'key': countyState, 'children': []}
+			sid = h[stateName]
+			states[sid]['children'].append(curcounty)
+			h[countyState] = len(states[sid]['children']) - 1
+		sid = h[stateName]
+		cid = h[countyState]
+		states[sid]['children'][cid]['children'].append(cur)
+	with open(jsonfilename, 'wb') as outfile:
+		json.dump(states, outfile)
+def prepare_treemap_json():
+	pass
+def prepare_linechart_csv():
+	pass
+if __name__ == "__main__":
+	#prepare_basic_source_data_csv()
+	prepare_filter_menu_bigO_json()
